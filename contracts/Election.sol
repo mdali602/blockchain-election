7a7f42f1d17e16c5806ef528e85d31744171ee15
mohdali.jck <mohd.ali@justclickkaro.com>
2019-03-24 16:44:39 +0530

    Done with list candidate part.

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,10 +1,30 @@
 pragma solidity ^0.5.0;
+// pragma solidity ^0.4.2;
 
 contract Election {
-// Store Candidate
-// Read Candidate
-  string public candidate;
+  // string public candidate;
+
+  // Model a Candidate
+  struct Candidate {
+    uint id;
+    string name;
+    uint voteCount;
+  }
+
+  // Store Candidate
+  // Fetch Candidate
+  mapping(uint => Candidate) public candidates;
+  // Store Candidate Count
+  uint public candidatesCount;
+
   constructor() public {
-    candidate = "Candidate 1";
+    // candidate = "Candidate 1";
+    addCandidate("Candidate 1");
+    addCandidate("Candidate 2");
+  }
+
+  function addCandidate (string memory _name) private {
+    candidatesCount++;
+    candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
   }
 }
